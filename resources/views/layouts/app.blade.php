968cb1c722ef257c51227f540b98a2c6d61a05be
MoorAdam <moor.adam.is@gmail.com>
2026-03-23 10:08:51 +0100

    layout components added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @livewireStyles
+</head>
+
+<body>
+    <x-ui.layout variant="header-sidebar">
+
+        <x-ui.layout.header class="px-6">
+            <x-slot:brand>
+                <x-ui.brand href="/dashboard" name="Vyzor" />
+            </x-slot:brand>
+
+            <x-ui.navbar>
+                
+            </x-ui.navbar>
+
+            <div class="ml-auto flex items-center gap-4">
+                <x-ui.avatar/>
+            </div>
+        </x-ui.layout.header>
+
+        <x-ui.sidebar>
+            <x-ui.navlist>
+                <x-ui.navlist.item label="Dashboard" icon="home" href="/dashboard" />
+                <x-ui.navlist.item label="Settings" icon="cog" href="/settings" />
+            </x-ui.navlist>
+        </x-ui.sidebar>
+
+        <x-ui.layout.main>
+            <div>
+                {{ $slot }}
+            </div>
+        </x-ui.layout.main>
+    </x-ui.layout>
+    @livewireScripts
+</body>
+
+</html>
