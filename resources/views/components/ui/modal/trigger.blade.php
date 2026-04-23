4cd559294a8fc566896b4b4179d8f0cb959f8013
MoorAdam <moor.adam.is@gmail.com>
2026-04-23 16:30:44 +0200

    user delete button now has permission constraints

--- resources/views/components/ui/modal/trigger.blade.php
+++ resources/views/components/ui/modal/trigger.blade.php
@@ -8,7 +8,7 @@
         @foreach (Arr::wrap($shortcut) as $item )
             x-on:keydown.{{ $item }}.prevent.window="$modal.open(@js($id))"
         @endforeach
-        x-on:click="$modal.open(@js($id))"
+        x-on:click="!$el.querySelector(':disabled') && $modal.open(@js($id))"
         {{ $attributes->merge(["modal-trigger [:where(&)]:inline cursor-pointer"]) }}
     >
         {{ $slot }}
